1fd9b28de2d2789a0430eeaf98cf12200acb076f
Mason <masontyler747@gmail.com>
2019-11-09 19:01:23 -0800

    pushing again

--- dataset_creator.py
+++ dataset_creator.py
@@ -1,3 +1,22 @@
 import numpy as np
 
 #iterate over the zip codes
+
+class ZipCode:
+    self.tiles = []
+    self.latitute = 0
+    self.longitutde = 0
+
+class Tile:
+    def __init__(self):
+        self.zip_codes = []
+        self.latitute = 0
+        self.longitutde = 0
+
+
+'''
+for z in zipCodes:
+    for t in Tiles:
+
+'''
+
